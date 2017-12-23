3b1a085bfebd6ca20d842098ec8df4e64435be92
Milan4e <psymilan@yahoo.com>
2017-12-23 18:55:56 +0100

    updates

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 const Koa = require('koa');
 const app = new Koa();
 const diacritics = require('diacritics')
-
+const random = require('random-words')
 
 // response 
 app.use(ctx => {
-  ctx.body = { name: 'Milan', message: diacritics.remove("ċḋċḋ") }
+  ctx.body = { name: 'Milan', message: random() }
 });
 
 app.listen(process.env.PORT || 3000);
