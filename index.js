111d306362e8b5dd4b9f62de4e13cf9f171ceaa2
Milan4e <psymilan@yahoo.com>
2017-12-23 18:21:53 +0100

    |

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+
+require('dotenv').config()
 const Koa = require('koa');
 const app = new Koa();
+const diacritics = require('diacritics')
+
  
 // response 
 app.use(ctx => {
-  ctx.body = process.env.MY_NAME;
+  ctx.body = diacritics.remove(process.env.MY_NAME);
 });
 
 app.listen(process.env.PORT || 3000);
--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www
+cd /var/api
 npm start
