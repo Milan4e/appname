3a28ec6668bc579a4afb5ba7d0ddb6f2eb2da38d
Milan4e <psymilan@yahoo.com>
2017-12-23 18:35:03 +0100

    fdsa

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const Koa = require('koa');
 const app = new Koa();
 const diacritics = require('diacritics')
 
- 
+
 // response 
 app.use(ctx => {
-  ctx.body = diacritics.remove(process.env.MY_NAME);
+  ctx.body = { name: 'Milan', message: diacritics.remove("ċḋḟġṁṗṡṫ") }
 });
 
 app.listen(process.env.PORT || 3000);
